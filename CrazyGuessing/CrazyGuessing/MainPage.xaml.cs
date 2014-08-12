9b90966f6694e78164dacdae30a353e2eba2b94d
Chang Yang <yang_chang@intsig.com>
2014-08-12 18:25:24 +0800

    - Add logic

--- CrazyGuessing/CrazyGuessing/MainPage.xaml.cs
+++ CrazyGuessing/CrazyGuessing/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using Microsoft.Devices.Sensors;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using CrazyGuessing.Resources;
@@ -13,29 +14,50 @@ namespace CrazyGuessing
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // 构造函数
+        Accelerometer _ac;
+
         public MainPage()
         {
             InitializeComponent();
 
-            // 用于本地化 ApplicationBar 的示例代码
-            //BuildLocalizedApplicationBar();
+            _ac = new Accelerometer();
+            _ac.ReadingChanged += new EventHandler<AccelerometerReadingEventArgs>(_ac_ReadingChanged);
         }
 
-        // 用于生成本地化 ApplicationBar 的示例代码
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // 将页面的 ApplicationBar 设置为 ApplicationBar 的新实例。
-        //    ApplicationBar = new ApplicationBar();
+        private void _ac_ReadingChanged(object sender, AccelerometerReadingEventArgs e)
+        {
+            //通过Dispatcher.BeginInvoke方法来更新UI，传入事件变量AccelerometerReadingEventArgs
+            Deployment.Current.Dispatcher.BeginInvoke(() => ProcessAccelerometerReading(e));
+        }
 
-        //    // 创建新按钮并将文本值设置为 AppResources 中的本地化字符串。
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
+        private bool isStart = true;
+        private int count = 0;
+        private bool isStatus = false;
 
-        //    // 使用 AppResources 中的本地化字符串创建新菜单项。
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        private void ProcessAccelerometerReading(AccelerometerReadingEventArgs e)
+        {
+            if (Math.Abs(e.Z) < 0.2 && isStart)
+            {
+                isStart = false;
+                MessageBox.Show("Start!");
+            }
+
+            if (isStart) return;
+
+            if (e.Z > 0.6) isStatus = true;
+            if (Math.Abs(e.Z) < 0.2 && isStatus)
+            {
+                isStatus = false;
+                count++;
+            }
+
+        }
+
+        private void M_1Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+
+            MPage1Panel.Visibility= Visibility.Collapsed;
+        }
     }
 }
